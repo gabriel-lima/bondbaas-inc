3dfed562f1669f8a50edd79432c16c55c9011813
Gabriel Lima <dev.gabriel.lima@gmail.com>
2019-04-10 18:51:01 -0300

    create middleware

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 )
 
 var db *sql.DB
@@ -25,21 +23,29 @@ func main() {
 }
 
 func tableHandler(w http.ResponseWriter, r *http.Request) {
-	table, id := extractPath(r.URL.Path)
-
-	if tableNameIsEmpty(table, w) || tableNameIsAReservedWord(table, w) || tableNameNotFound(table, w) {
+	adminGateway := storage.AdminGateway{DB: db}
+	middleware := handlers.Middleware{
+		Request:      r,
+		Response:     w,
+		AdminGateway: adminGateway,
+	}
+	tableName, ID, invalid := middleware.Route()
+	if invalid {
 		return
 	}
 
-	gateway := storage.TableGateway{DB: db, Table: table}
+	tableGateway := storage.TableGateway{
+		DB:    db,
+		Table: tableName,
+	}
 	handler := handlers.TableHandler{
 		Request:      r,
 		Response:     w,
-		TableGateway: gateway,
+		TableGateway: tableGateway,
 	}
 
 	if r.Method == "GET" {
-		handler.Get(id)
+		handler.Get(ID)
 	}
 
 	if r.Method == "POST" {
@@ -47,64 +53,12 @@ func tableHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == "PUT" {
-		handler.Update(id)
+		handler.Update(ID)
 	}
 
 	if r.Method == "DELETE" {
-		handler.Delete(id)
+		handler.Delete(ID)
 	}
-}
-
-func extractPath(path string) (tableName string, ID int) {
-	paths := strings.Split(path, "/")
-	deleteEmptyPaths(paths)
-
-	tableName = strings.ToLower(strings.Join(paths[1:], ""))
-	ID, _ = strconv.Atoi(strings.Join(paths[2:], ""))
-
-	return tableName, ID
-}
-
-func deleteEmptyPaths(s []string) {
-	var r []string
-	for _, str := range s {
-		if str != "" {
-			r = append(r, str)
-		}
-	}
-}
-
-func tableNameIsEmpty(tableName string, w http.ResponseWriter) bool {
-	if tableName == "" {
-		http.Error(w, "Please inform a table", 404)
-		return true
-	}
-	return false
-}
-
-func tableNameIsAReservedWord(tableName string, w http.ResponseWriter) bool {
-	if tableName == "admin" {
-		http.Error(w, "admin is a reserved name.", 422)
-		return true
-	}
-	return false
-}
-
-func tableNameNotFound(tableName string, w http.ResponseWriter) bool {
-	gateway := storage.AdminGateway{DB: db}
-
-	hasTable, err := gateway.HasTable(tableName)
-	if err != nil {
-		responseInternalError(w, err)
-		return true
-	}
-
-	if !hasTable {
-		http.Error(w, fmt.Sprintf("Table %s not found.", tableName), 404)
-		return true
-	}
-
-	return false
 }
 
 /// Create a table schema
@@ -123,11 +77,11 @@ POST
 }
 */
 func adminHandler(w http.ResponseWriter, r *http.Request) {
-	gateway := storage.AdminGateway{DB: db}
+	adminGateway := storage.AdminGateway{DB: db}
 	handler := handlers.AdminHandler{
 		Request:      r,
 		Response:     w,
-		AdminGateway: gateway,
+		AdminGateway: adminGateway,
 	}
 
 	if r.Method == "GET" {
@@ -137,14 +91,4 @@ func adminHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		handler.Create()
 	}
-}
-
-func responseMalformed(w http.ResponseWriter, err error) {
-	http.Error(w, err.Error(), 422)
-	log.Println(err)
-}
-
-func responseInternalError(w http.ResponseWriter, err error) {
-	http.Error(w, err.Error(), 500)
-	log.Println(err)
 }
